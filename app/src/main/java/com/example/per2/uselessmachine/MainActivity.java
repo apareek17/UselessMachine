999b4ef2a836707157ed85a1fe800d94f565d48d
Ayush Pareek <ayush7702@gmail.com>
2018-08-29 08:49:57 -0700

    Added countdowntimer to switch to turn off after 5 seconds. Switching off early cancels the timer.

--- app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.per2.uselessmachine;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +30,33 @@ public class MainActivity extends AppCompatActivity {
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
                         if (isChecked) {
-                            Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
+                            startSwitchOffTimer();
+                            //Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(MainActivity.this, "Off", Toast.LENGTH_SHORT).show();
                         }
                     }
 
                 });
+    }
+
+    private void startSwitchOffTimer() {
+        new CountDownTimer(5000, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if(!switchUseless.isChecked())
+                {
+                    //Log.d(TAG, "onTick: canceling");
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                switchUseless.setChecked(false);
+                //Log.d(TAG, "onFinish: switch set to false");
+            }
+        }.start();
+
     }
 
     private void wireWidgets() {
