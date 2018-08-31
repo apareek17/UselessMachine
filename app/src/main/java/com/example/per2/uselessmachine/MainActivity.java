f96cdeb32c0a57138b85e682671c71a79ac480a7
Ayush Pareek <ayush7702@gmail.com>
2018-08-31 08:10:43 -0700

    Constructed a self destruct timer.

--- app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -24,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setListeners() {
+        buttonSelfDestruct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSelfDestructSequence();
+            }
+        });
+
+
         switchUseless.setOnCheckedChangeListener(
                 new CompoundButton.OnCheckedChangeListener() {
                     @Override
@@ -32,12 +41,28 @@ public class MainActivity extends AppCompatActivity {
                         if (isChecked) {
                             startSwitchOffTimer();
                             //Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(MainActivity.this, "Off", Toast.LENGTH_SHORT).show();
                         }
                     }
 
                 });
+    }
+
+    private void startSelfDestructSequence() {
+        buttonSelfDestruct.setEnabled(false);
+        new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                buttonSelfDestruct.setText("Destruct in " + millisUntilFinished/1000);
+
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+
+            }
+        }.start();
+
     }
 
     private void startSwitchOffTimer() {
